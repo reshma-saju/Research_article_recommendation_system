c58d5c68c8e8a1110f6ab545b18a637f59484d48
reshma-saju <86281005+reshma-saju@users.noreply.github.com>
2023-06-12 04:18:07 +0530

    Trying to implement flask (1)
    Files are being moved here and there, make note to rearrange them later

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,25 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
-@app.route("/bakabaka")
-def hi():
-    return "Hi dummy"
+# @app.route('/hello/<title>')
+# def hello(user):
+#    return render_template('hello.html', name = user)
+
+@app.route('/recommendation',methods = ['POST', 'GET'])
+def reco():
+    title = request.form['inp_title']
+    abst = request.form['inp_abstract']
+    if request.method == 'POST':
+      return render_template('hello.html', name = title)
+    else:
+      return redirect(url_for('success',name = title))
+
+
+
+
+if __name__ == '__main__':
+    app.run()
