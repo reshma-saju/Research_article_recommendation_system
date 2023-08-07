c855ae6c0085ba8cc05d2ac10846e5f60e6ef6b5
reshma-saju <86281005+reshma-saju@users.noreply.github.com>
2023-08-07 23:57:55 +0530

    improved LSA algo

--- backend/models/titlestopaper.py
+++ backend/models/titlestopaper.py
@@ -1,42 +1,42 @@
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
-
-# title_read="""Matter-Wave Bright Solitons with a Finite Background in Spinor
-#   Bose-Einstein Condensates
-# """
 
 
-def title_getRecommendation(title_read):
+def title_getRecommendation(input_title):
 
-    RES_ARTICLES = ".\data\project_data.csv"
-    # ARTICLE_READ=[1,10]
+    # Load dataset
+    data = pd.read_csv('.\data\project_data.csv')
+    titles = data['title']
+
+    # Text preprocessing and creating a document-term matrix
+    vectorizer = TfidfVectorizer(
+        stop_words='english', max_df=0.8, min_df=2, ngram_range=(1, 2))
+    X = vectorizer.fit_transform(titles)
+
+    # Apply Latent Semantic Analysis (LSA)
+    num_topics = 32  # Experiment with different numbers
+    lsa_model = TruncatedSVD(n_components=num_topics)
+    lsa_topic_matrix = lsa_model.fit_transform(X)
+
+    # Normalize LSA topic matrix
+    from sklearn.preprocessing import normalize
+    lsa_topic_matrix = normalize(lsa_topic_matrix)
+
+    # Function to get recommendations
     num_recommendations = 5
+    input_vector = vectorizer.transform([input_title])
+    input_topic = lsa_model.transform(input_vector)
+    input_topic = normalize(input_topic)
 
-    res_articles = pd.read_csv(RES_ARTICLES, encoding='utf-8', index_col=0)
-    # drop all the unnamed columns
-    res_articles.drop(res_articles.columns[res_articles.columns.str.contains(
-        'unnamed', case=False)], axis=1, inplace=True)
-    res_articles.head()
-    res_ids = res_articles.index.tolist()
+    similarities = cosine_similarity(input_topic, lsa_topic_matrix)
 
-    res_articles = res_articles[['title', 'abstract']].dropna()
-    # articles is a list of all articles
-    titles = res_articles['title'].tolist()
-    titles = [title_read]+titles
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform(titles)
-    # Choose the desired number of components
-    lsa_model = TruncatedSVD(n_components=100)
-    lsa_matrix = lsa_model.fit_transform(tfidf_matrix)
-    similarity_matrix = cosine_similarity(lsa_matrix)
-    input_text_index = 0  # Index of the input text
-    similar_text_indices = similarity_matrix[input_text_index].argsort(
-    )[-(num_recommendations+1):][::-1][1:]  # Exclude the input text
-    ref_indices = similar_text_indices
-    similar_text_indices = [res_ids[i-2] for i in similar_text_indices]
-    recommended_texts = [titles[idx-1] for idx in ref_indices]
+    similar_indices = similarities.argsort(
+    )[0][-num_recommendations-1:-1][::-1]
+    similar_text_indices = []
+    for index in similar_indices:
+        similar_text_indices.append(data['id'][index])
     for i, val in enumerate(similar_text_indices):
         if len(str(val)) < 10:
             num_str = str(val)
@@ -45,6 +45,13 @@ def title_getRecommendation(title_read):
             whole_part = whole_part.rjust(4, '0')
             decimal_part = decimal_part.ljust(4, '0')
 
-            similar_text_indices[i] = whole_part + '.' + decimal_part
-    # results is in print(recommended_texts)
-    return dict(zip(similar_text_indices, recommended_texts))
+            similar_text_indices[i]=whole_part + '.' + decimal_part
+
+    recommendations = {}
+    j=0
+    for index in similar_text_indices:
+        recommendations[index] = data['title'][similar_indices[j]]
+        j+=1
+
+    # Test the recommendation syste
+    return recommendations
